d778e024343f06e88aba3daa515263aaa40bbf9c
NaCarol0 <goncalves.lopes.ana@escola.pr.gov.br>
2025-08-05 10:24:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function draw() {
   background(363,125,340); // cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(440, 300); //esfera 3D
+  sphere(440, 500); //esfera 3D
   
   
 }
